9bf80ae1308d2a3b4884a1425b279f8e97579260
Tomoe-12 <li8993han@gmail.com>
2025-03-27 19:06:20 +0630

    again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+export default tseslint.config({
+   "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    ...compat('next/core-web-vitals', 'next/typescript')
+})
