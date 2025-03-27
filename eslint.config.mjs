2d02794c164dd65ff7c767348dcd177254d68ff9
Tomoe-12 <li8993han@gmail.com>
2025-03-27 16:26:06 +0630

    again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,16 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "next/inline-script-id": "off",  // Example rule
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-html-link-for-pages": "off"
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
+  }),
+]
 
 export default eslintConfig;
-
