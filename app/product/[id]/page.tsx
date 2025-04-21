9b1766a496c29f8e1594809ec7e734483a3e52c9
Tomoe-12 <li8993han@gmail.com>
2025-04-21 18:24:58 +0630

    refactor: enhance database integration and query logic in product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,11 +1,13 @@
-
 import CareInstructions from '@/components/home/careInstructions'
 import ContactUs from '@/components/home/contactUs'
 import { ImageSlider } from '@/components/home/imageSlider'
 import OurLink from '@/components/home/ourLink'
 import ProductDetails from '@/components/home/productDetils'
 import SizeChart from '@/components/home/table/sizeChart'
+import { db } from '@/server'
 import React from 'react'
+import {products} from '@/server/schema'
+import { eq } from 'drizzle-orm'
 
 type Props = {
     params : Promise<{id : string}>
@@ -16,9 +18,18 @@ const page = async({params} : Props) => {
     const {id} = await params 
     // const ProductId = Number(id) // convert to number
     console.log('id',id);
-    
 
-    
+
+    const productsInfo = await db.query.products.findFirst({
+      where : eq(products.id,Number(id)),
+      with : {
+        images:true , 
+        cares : true 
+      }
+    })
+
+    console.log('productsInfo',productsInfo);
+     
 
   return (
     <main className="flex flex-col gap-5 sm:gap-7 md:gap-9">
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/server/schema";
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+export const db = drizzle(sql, { schema, logger: true });
 
 // const result = await db.execute('select 1');
